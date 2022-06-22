00129602e1d126baded2ae6ef2ef642e6b84f8fa
danielxf007 <daniel.higuita007@gmail.com>
2022-06-22 12:43:05 -0500

    added createUser func

--- build/18.4.0/loadDump.sh
+++ build/18.4.0/loadDump.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+createUser(){
+sqlplus -s / as sysdba << EOF
+alter session set "_ORACLE_SCRIPT"=true;  
+create user $1
+identified by $2
+default tablespace BUPP
+temporary tablespace temp 
+profile default;
+exit;
+EOF
+}
+
 sqlplus -s / as sysdba << EOF
 create tablespace BUPP
 default storage (
@@ -24,7 +36,7 @@ pctincrease 0
 )
 permanent
 datafile
-'indices_01.dbf' size 50m
+'idx_01.dbf' size 50m
 autoextend on next 50m maxsize 4000m
 ;
 create directory siiu_pump_dir as '/tmp/dump';
@@ -48,11 +60,23 @@ grant read, write on directory siiu_pump_dir to $SIIU_USER;
 
 exit;
 EOF
-IFS=', ' read -r -a array <<< "$DUMP_FILES"
-for element in "${array[@]}"
+
+IFS=', ' read -r -a array <<< "$SIIU_USERS"
+for user in "${array[@]}"
 do
-    echo "Loading dump file $element"
-    impdp system/$ORACLE_PWD@localhost:1521 schemas=bupp directory=siiu_pump_dir dumpfile=$element version=11.2.0.4.0 
+    echo "Creating $user"
+    createUser $user $ORACLE_PWD
+done
+
+IFS=', ' read -r -a array <<< "$DUMP_FILES"
+for dump_file in "${array[@]}"
+do
+    echo "Loading dump file $dump_file"
+    impdp system/$ORACLE_PWD@localhost:1521 tables=BUPP.SIIU_PROYECTO,BUPP.SIIU_CONVOCATORIA \
+    directory=siiu_pump_dir dumpfile=$dump_file data_options=skip_constraint_errors version=11.2.0.4.0
+    #query='BUPP.SIIU_PROYECTO:"where rownum <= (select count(*)/2 from BUPP.SIIU_PROYECTO)"' \
     #logfile=$DUMP_LOG_FILE 
 done
+
+
 
